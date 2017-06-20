46a26b6b77a6506b1bfea333120714a14799ec71
chulin <316014408@qq.com>
2017-06-20 17:15:25 +0800

    .

--- const.go
+++ const.go
@@ -4,6 +4,7 @@ const (
 	URL_HTTPS         string = "https://eco.taobao.com/router/rest"
 	URL_HTTP          string = "http://gw.api.taobao.com/router/rest"
 	Method_SendSMS    string = "alibaba.aliqin.fc.sms.num.send"
+	Method_QuerySMS   string = "alibaba.aliqin.fc.sms.num.query"
 	Method_CallTTS    string = "alibaba.aliqin.fc.tts.num.singlecall"
 	Method_CallVoice  string = "alibaba.aliqin.fc.voice.num.singlecall"
 	Method_CallDouble string = "alibaba.aliqin.fc.voice.num.doublecall"
